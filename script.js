502d51db570a283bf51bfecc507ff6b93fa250a4
coreyj <9023272+alpine-ocean@users.noreply.github.com>
2025-12-27 13:50:35 +0000

    Build get choice functions
    Built getComputerChoice and getHumanChoice functions. Started playRound
    but not complete.

---
+++ script.js
@@ -0,0 +1,46 @@
+let computerScore = 0;
+let humanScore = 0;
+
+
+
+function getComputerChoice() {
+    
+    let choice = 3 * Math.random();
+    
+    if (choice <= 1) {
+        choice = 'rock';
+    } else if (choice >1 && choice <=2) {
+        choice = 'paper';
+    } else {
+        choice = 'scissor';
+    }
+
+    return(choice)
+}
+
+/*console.log(getComputerChoice())*/
+
+function getHumanChoice() {
+    let humanChoice = prompt('rock?, paper?, scissor?:')
+    return(humanChoice.toLowerCase());
+}
+
+/*console.log(getHumanChoice())*/
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == 'rock' && computerChoice == 'scissor') {
+        humanScore = humanScore + 1;
+        console.log(`You Win! ${humanChoice} beats ${computerChoice}`)
+    } else if (humanChoice == 'scissor' && computerChoice == 'paper') {
+        humanScore = humanScore + 1;
+        console.log(`You Win! ${humanChoice} beats ${computerChoice}`)
+    } else if (humanChoice == 'paper' && computerChoice == 'rock') {
+        humanScore = humanScore + 1;
+        console.log(`You Win! ${humanChoice} beats ${computerChoice}`)
+    } else if (humanChoice == computerChoice) {
+        console.log('Tie, please play again!')
+    }  else {
+        computerScore = computerScore + 1;
+        console.log(`You Lose! ${computerChoice} beats ${humanChoice}`)
+    }
+}
