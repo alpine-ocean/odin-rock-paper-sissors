da7e4118fcc96acd3852001d2d94b6bd9cba91c6
coreyj <9023272+alpine-ocean@users.noreply.github.com>
2025-12-28 00:43:25 +0000

    Develop playGame function
    Developed playGame function. Brut force method without using a loop.
    will return to refine it in the future. Game is now functional and
    can be played directly in the console.

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let computerScore = 0;
-let humanScore = 0;
-
 
 
 function getComputerChoice() {
@@ -27,27 +24,62 @@ function getHumanChoice() {
 
 /*console.log(getHumanChoice())*/
 
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice == 'rock' && computerChoice == 'scissor') {
-        humanScore = humanScore + 1;
-        console.log(`You Win! ${humanChoice} beats ${computerChoice}`)
-    } else if (humanChoice == 'scissor' && computerChoice == 'paper') {
-        humanScore = humanScore + 1;
-        console.log(`You Win! ${humanChoice} beats ${computerChoice}`)
-    } else if (humanChoice == 'paper' && computerChoice == 'rock') {
-        humanScore = humanScore + 1;
-        console.log(`You Win! ${humanChoice} beats ${computerChoice}`)
-    } else if (humanChoice == computerChoice) {
-        console.log('Tie, please play again!')
-    }  else {
-        computerScore = computerScore + 1;
-        console.log(`You Lose! ${computerChoice} beats ${humanChoice}`)
+
+function playGame() {
+    let computerScore = 0;
+    let humanScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice == 'rock' && computerChoice == 'scissor') {
+            humanScore = humanScore + 1;
+            console.log(`You Win! ${humanChoice} beats ${computerChoice}`)
+        } else if (humanChoice == 'scissor' && computerChoice == 'paper') {
+            humanScore = humanScore + 1;
+            console.log(`You Win! ${humanChoice} beats ${computerChoice}`)
+        } else if (humanChoice == 'paper' && computerChoice == 'rock') {
+            humanScore = humanScore + 1;
+            console.log(`You Win! ${humanChoice} beats ${computerChoice}`)
+        } else if (humanChoice == computerChoice) {
+            console.log('Tie!')
+        }  else {
+            computerScore = computerScore + 1;
+            console.log(`You Lose! ${computerChoice} beats ${humanChoice}`)
+        }
+
     }
-    console.log(humanChoice)
-    console.log(computerChoice)
+
+    /*Round 1*/
+    playRound(getComputerChoice(), getHumanChoice())
+    console.log(`Computer Score: ${computerScore}`)
+    console.log(`Human Score: ${humanScore}`)
+
+    /*Round 2*/
+    playRound(getComputerChoice(), getHumanChoice())
+    console.log(`Computer Score: ${computerScore}`)
+    console.log(`Human Score: ${humanScore}`)
+
+    /*Round 3*/
+    playRound(getComputerChoice(), getHumanChoice())
+    console.log(`Computer Score: ${computerScore}`)
+    console.log(`Human Score: ${humanScore}`)
+
+    /*Round 4*/
+    playRound(getComputerChoice(), getHumanChoice())
+    console.log(`Computer Score: ${computerScore}`)
+    console.log(`Human Score: ${humanScore}`)
+
+    /*Round 5*/
+    playRound(getComputerChoice(), getHumanChoice())
+    console.log(`Computer Score: ${computerScore}`)
+    console.log(`Human Score: ${humanScore}`)
+
+    if (humanScore > computerScore) {
+        console.log("Congratulations, you win!!");
+    } else if (humanScore == computerScore) {
+        console.log("Tie, please try again!!");
+    } else {
+        console.log("You lose, try again!!");
+    }
 }
 
-console.log(playRound(getComputerChoice(), getHumanChoice()))
-
-console.log(computerScore)
-console.log(humanScore)
+playGame()
